373ee874c1fab1d92993edc578105ceb3a1636b7
Foster Yin <fyin@bbtechgroup.com>
2014-04-23 09:26:43 +0800

    [ios] Add:

--- if_statement.m
+++ if_statement.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: if
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: D70E6D11-0297-4BAB-88AA-86D5D5CBBC5D
+// IDECodeSnippetIdentifier: 4DD46FA6-9A94-4F45-AB57-FF123DCA2A44
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 if (<#condition#>) {
     <#statements#>
 }
