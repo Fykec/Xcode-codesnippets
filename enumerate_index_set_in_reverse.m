15210fa937c96bc67b690c6b3f9587bf3ef6b9bf
Foster Yin <fyin@bbtechgroup.com>
2014-04-23 09:13:48 +0800

    [ios] Add:

--- enumerate_index_set_in_reverse.m
+++ enumerate_index_set_in_reverse.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: 
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: 6F7768D1-F92C-42EB-986B-AF5DB06CD076
+// IDECodeSnippetIdentifier: 2AACE753-0D6B-478C-A800-F183970D2C42
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 NSUInteger index = [<#indexSet#> lastIndex];
 while (index != NSNotFound) {
     <#statements#>
