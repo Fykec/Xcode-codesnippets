8f830f3d403f04d5f4f62745df8db80a2397b1c9
Foster Yin <fyin@bbtechgroup.com>
2014-04-23 09:17:06 +0800

    [ios] Add:

--- objective-c_category.m
+++ objective-c_category.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: @interface-category
 // IDECodeSnippetCompletionScopes: [TopLevel]
-// IDECodeSnippetIdentifier: 4E041FB2-AB0D-4249-9658-141289641F6B
+// IDECodeSnippetIdentifier: 7962B57E-37B3-4E78-882D-41E527556EDF
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 @interface <#class name#> (<#category name#>)
 
 @end
