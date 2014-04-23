e5f8a95d1e57e6c98d720b4cbb5f6b106cee5763
Foster Yin <fyin@bbtechgroup.com>
2014-04-23 09:22:34 +0800

    [ios] Add:

--- core_data_to-many_relationship_accessors.m
+++ core_data_to-many_relationship_accessors.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: 
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: C1E91503-E91F-4EEA-A032-D98D80CDD00A
+// IDECodeSnippetIdentifier: 7766BE6C-0BD6-4C83-AC90-6FA220B615C9
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 - (void)add<#CapitalizedRelationshipName#>Object:(<#relationship destination class#> *)value
 {    
     NSSet *changedObjects = [NSSet setWithObject:value];
