a1c6b26a917a90e7252979c9202597e469774009
Foster Yin <fyin@bbtechgroup.com>
2014-04-23 09:25:39 +0800

    [ios] Add:

--- enumerated_type_declaration_(ns_options).m
+++ enumerated_type_declaration_(ns_options).m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: nsoptions
 // IDECodeSnippetCompletionScopes: [CodeBlock,TopLevel]
-// IDECodeSnippetIdentifier: 247A1538-3248-42C0-94DC-4332862B6625
+// IDECodeSnippetIdentifier: EB51A5EA-076F-456C-A736-D1D44A737C7D
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 typedef NS_OPTIONS(NSUInteger, <#MyEnum#>) {
     <#MyEnumValueA#> = 1 << 0,
     <#MyEnumValueB#> = 1 << 1,
