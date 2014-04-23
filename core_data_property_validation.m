be4bde7c7b248b0c96edee5f416291bf32e7bd3c
Foster Yin <fyin@bbtechgroup.com>
2014-04-23 09:26:50 +0800

    [ios] Add:

--- core_data_property_validation.m
+++ core_data_property_validation.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: 
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: B73B46A2-E9E4-4D78-9365-B537727CE602
+// IDECodeSnippetIdentifier: 030D8445-68E4-4EB9-B6C5-DA8AC339A828
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 - (BOOL)validate<#CapitalizedPropertyName#>:(id *)valueRef error:(NSError **)outError
 {
     BOOL validationResult = YES;
