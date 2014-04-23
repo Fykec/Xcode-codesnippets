9db2925fa0d869d143500e2c0e13242608d5ce88
Foster Yin <fyin@bbtechgroup.com>
2014-04-23 09:13:58 +0800

    [ios] Add:

--- objective-c_protocol_definition.m
+++ objective-c_protocol_definition.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: @protocol
 // IDECodeSnippetCompletionScopes: [TopLevel]
-// IDECodeSnippetIdentifier: 986936BB-E0FE-4E7E-9A79-E3C77528204E
+// IDECodeSnippetIdentifier: 0A543024-29CD-4A8A-B796-8E08EF6F6935
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 @protocol <#protocol name#> <NSObject>
 
 <#methods#>
