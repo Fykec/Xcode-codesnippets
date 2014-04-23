6491d7d74abf0a5797906e270c658adc7fce997d
Foster Yin <fyin@bbtechgroup.com>
2014-04-23 09:27:13 +0800

    [ios] Add:

--- objective-c_-description_method.m
+++ objective-c_-description_method.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: description
 // IDECodeSnippetCompletionScopes: [ClassImplementation]
-// IDECodeSnippetIdentifier: 16D98350-143B-4CC9-9EA4-57525DAD8D74
+// IDECodeSnippetIdentifier: 6823D414-36F4-4A80-A30A-6E7FCF4F9B9B
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 - (NSString *)description
 {
     return [NSString stringWithFormat:@"<#format string#>", <#arguments#>];
