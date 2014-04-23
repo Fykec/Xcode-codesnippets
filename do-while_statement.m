1ea7700bb0cf25fae959e43cc9089cfbb799b296
Foster Yin <fyin@bbtechgroup.com>
2014-04-23 09:25:48 +0800

    [ios] Add:

--- do-while_statement.m
+++ do-while_statement.m
@@ -3,10 +3,10 @@
 //
 // IDECodeSnippetCompletionPrefix: dowhile
 // IDECodeSnippetCompletionScopes: [CodeBlock]
-// IDECodeSnippetIdentifier: 1FA9842C-8454-42F7-9119-5818388EDB58
+// IDECodeSnippetIdentifier: E9D010E6-336E-40CD-A6C1-96C658134128
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
 // IDECodeSnippetUserSnippet: 1
-// IDECodeSnippetVersion: 1
+// IDECodeSnippetVersion: 2
 do {
     <#statements#>
 } while (<#condition#>);
