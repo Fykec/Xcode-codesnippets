e4fa9cccc922b46a792334ffca8f8a701ba236c3
Foster Yin <fyin@bbtechgroup.com>
2014-04-24 16:39:50 +0800

    [ios] Add:

---
+++ racsignalapi.m
@@ -0,0 +1,17 @@
+// racsignalapi
+// 
+//
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 8E2C11CE-A97E-41FD-911E-7FAF6EB04CE2
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+RACSignal *signal = [[BBAPIManger sharedInstance] ####] objectForKey:kID]];
+
+    [signal subscribeNext:^(id resp) {
+
+    }];
+
+    [signal subscribeError:^(NSError *error) {
+
+    }];
